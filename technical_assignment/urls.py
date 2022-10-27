f8e1f449078f6963aaf801dec1e277b6ccc20c00
Kegan Overberg <keganoverberg@Kegans-MacBook-Air.local>
2022-10-27 15:39:04 +0200

    added tailwind to project

--- technical_assignment/urls.py
+++ technical_assignment/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("users/", include("users.urls"))
+    path("users/", include("users.urls")),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]

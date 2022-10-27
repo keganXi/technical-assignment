818fd922a8df589c5d4310ebf3746387d11896d1
Kegan Overberg <keganoverberg@Kegans-MacBook-Air.local>
2022-10-27 12:25:46 +0200

    change names of env variables in ci database config

--- technical_assignment/settings/ci.py
+++ technical_assignment/settings/ci.py
@@ -30,9 +30,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("POSTGRES_DB"),
-        'USER': os.environ.get("POSTGRES_USER"),
-        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'NAME': os.environ.get("DATABASE_NAME"),
+        'USER': os.environ.get("DATABASE_USER"),
+        'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
         'HOST': "localhost",
         'PORT': 5432
     }

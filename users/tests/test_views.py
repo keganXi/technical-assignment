a82eb4e017f0a33b54d860315e071087dd6f060b
Kegan Overberg <keganoverberg@Kegans-MacBook-Air.local>
2022-10-28 14:35:30 +0200

    changed url variable names in users/test_view.py

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -16,12 +16,20 @@ from users.models import User
 class TestRegisterView(TestRegsterForm):
 
     def setUp(self):
-        self.url = reverse("register")
+        self.url_register = reverse("register")
+        self.url_login = reverse("login")
 
     def test_register_user_successful(self):
         # send post request with user registration data.
-        response = self.client.post(self.url, data=self.users[0])
+        response = self.client.post(self.url_register, data=self.users[0])
         self.assertEquals(response.status_code, 302) # post request was successful and redirect to login.
         username = self.users[0]["username"] # get username.
         self.assertTrue(User.objects.filter(username=username).exists()) # check if user was created.
+
+
+    def test_login_user_successful(self):
+        # send post request with user login data.
+        response = self.client.post(self.url_login, data=self.users[0])
+        self.assertEquals(response.status, 302) # post request was successful and redirect to home.
+        
         

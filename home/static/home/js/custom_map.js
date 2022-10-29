5d1d41cd71d1924a4751ee672a4aab0d3865b1a9
Kegan Overberg <keganoverberg@Kegans-MacBook-Air.local>
2022-10-29 16:19:59 +0200

    set marker to my location

--- home/static/home/js/custom_map.js
+++ home/static/home/js/custom_map.js
@@ -2,9 +2,17 @@
 
 // NOTE: Building leaftletJS map for displaying registered users.
 
-var map = L.map("map").setView([51.505, -0.09], 13);
+var latt = -34.04;
+var long = 18.60;
+
+// Map view.
+var map = L.map("map").setView([latt, long], 10);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+// add markers to map.
+var marker = L.marker([latt, long]).addTo(map);
